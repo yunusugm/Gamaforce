09e9c83fc464478621b33a555cad1a52fd95627c
Yunus Alif <84793337+yunusanr@users.noreply.github.com>
2022-11-26 11:08:58 +0700

    Tambah komentar di fungsi MPU

--- Github/Github.ino
+++ Github/Github.ino
@@ -9,31 +9,32 @@ void loop() {
 }
 
 void mpu6050_setup() {
+  // RESET MPU
   Wire.beginTransmission(MPU);
   Wire.write(MPU);
   Wire.write(0b00000111);
   Wire.endTransmission();
-  //mpu reset
+  // RESET BACK TO 0
   Wire.beginTransmission(0x68);
   Wire.write(MPU);
   Wire.write(0b00000000);
   Wire.endTransmission();
-  // low pass filter
+  // DLPF
   Wire.beginTransmission(0x68);
   Wire.write(0x1A);
   Wire.write(0b00000001);
   Wire.endTransmission();
-  // powe management / reset
+  // POWER MANAGEMENT ENABLE TEMP
   Wire.beginTransmission(MPU);
   Wire.write(0x6B);
   Wire.write(0x00);
   Wire.endTransmission();
-  // acelero dan high pass filter
+  // Konfigurasi akselerometer dengan full scale range 2g + DHPF
   Wire.beginTransmission(MPU);
   Wire.write(0x1C);
   Wire.write(0b00000111);
   Wire.endTransmission();
-  // gyro reset
+  // Konfigurasi gyroscope dengan full scale range 250 deg/s
   Wire.beginTransmission(MPU);
   Wire.write(0x1B);
   Wire.write(0x00);
